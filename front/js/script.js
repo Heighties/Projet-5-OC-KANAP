e6514d19a40f2731e795003a8481b3bbf6b67f44
Heighties <heighties@gmail.com>
2022-06-14 01:46:40 +0200

    index products

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,57 @@
 
-    const apiUrl =`http://localhost:3000/api/products`;
+    // const apiUrl =`http://localhost:3000/api/products`;
     // async function getProducts(){
     //     const response = await fetch(apiUrl);
     //     const data = await response.json();
     //     console.log(data);
     // }
 
-    async function getProducts(){
-    fetch(apiUrl).then((response) => response.json().then((data) => {
-        console.log(data); 
-    }
-    ))}
-    getProducts();
+    // async function getProducts(){
+    // fetch(apiUrl).then((response) => response.json().then((data) => {
+    //     console.log(data); 
+    // }
+    // ))}
+    // getProducts();
 
-    let a = document.createElement('a');
-    let article = document.createElement('article');
-    let prdName = document.createElement('h3')
+    // let a = document.createElement('a');
+    // let article = document.createElement('article');
+    // let prdName = document.createElement('h3')
 
-    document.getElementById("items").appendChild(a);
-    a.appendChild(article);
-    article.appendChild(prdName);
-    prdName.classList.add("productName");
+    // document.getElementById("items").appendChild(a);
+    // a.appendChild(article);
+    // article.appendChild(prdName);
+    // prdName.classList.add("productName");
     // prdName.innerHTML = "TEST";
-    prdName.textContent = data.name;
+   
+    fetch(`http://localhost:3000/api/products`).then(function(res){
+        if(res.ok){
+            return res.json();
+        }
+        console.log(res);
+    })
+    .then(function(datas){
+        datas.forEach(product =>{
+            let items = document.getElementById('items');
+            a = document.createElement('a');
+            article = document.createElement('article');
+            article.classList.add("productCard");
+            img = document.createElement('img');
+            img.classList.add("productImage");
+            h3 = document.createElement('h3');
+            h3.classList.add("productName");
+            p = document.createElement('p');
+            p.classList.add("productDescription");
+
+            items.appendChild(a);
+            a.appendChild(article);
+            article.appendChild(img);
+            article.appendChild(h3);
+            article.appendChild(p);
+
+            a.href = product._id;
+            img.src = product.imageUrl;
+            img.alt = product.altTxt;
+            h3.innerHTML = product.name;
+            p.innerHTML = product.description;
+        });
+    });
